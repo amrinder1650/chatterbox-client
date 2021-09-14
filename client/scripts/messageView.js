f1a1ccc8029bfc52a89cd809619978a1e956d7a1
Amrinder Singh <amrinder1650@gmail.com>
2021-09-13 20:45:04 -0700

    Working on Complete displaying messages

--- client/scripts/messageView.js
+++ client/scripts/messageView.js
@@ -8,7 +8,7 @@ var MessageView = {
   render: _.template(`
       <!--
       <div class="chat">
-        <div class="username"></div>
+        <div class="username"> <%-username%>   </div>
         <div></div>
       </div>
       -->
